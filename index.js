b8574288de52e67517836aaf0bfd718c5842a96c
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-01-02 14:59:10 +0200

    new element in mdb compass

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt'
+
+import {registerValidator} from './validation/auth.js'
+import { validationResult } from 'express-validator';
+import UserModel from './models/User.js'
 
 mongoose.connect(
-    'mongodb+srv://auth:18102000@cluster0.g2sluk8.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://auth:18102000@cluster0.g2sluk8.mongodb.net/address?retryWrites=true&w=majority'
 ).then(()=>{
     console.log("DB ok")
 }).catch((err)=>console.log("DB ERR", err))
@@ -12,24 +17,27 @@ const app =express();
 
 app.use(express.json())
 
-app.get('/',(req, res)=>{
-    res.send("hello jfjfj")
-})
 
-app.post('/auth/login',(req,res)=>{
-    console.log(req.body)
-    const token =jwt.sign(
-        {
-            email:req.body.email,
-            fullName:'Tom Felton'
-        },
-        'secret123',
-    )
-    res.json({
-        success:true,
-        token,
+app.post('/auth/register', registerValidator, async(req,res)=>{
+    const errors =validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json(errors.array())
+    }
+    const password=req.body.password;
+    const solt=await bcrypt.genSalt(10)
+    const passwordHash=await bcrypt.hash(password,solt)
+
+    const doc=new UserModel({
+        email:req.body.email,
+        fullName:req.body.fullName,
+        passwordHash,
     })
-})
+
+    const user=await doc.save();
+
+    res.json(user)
+
+});
 
 app.listen(2000, (err)=>{
     if(err){
